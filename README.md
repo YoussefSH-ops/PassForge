7225365e0ffb0284cacc3e9923d4f2e64ca22ca2
YoussefSH-ops <youssefchamas03@gmail.com>
2026-02-09 10:02:16 +0200

    argparsing + letters config

--- README.md
+++ README.md
@@ -4,32 +4,42 @@ Targeted password worldlist generator for security testing and CTFs
 
 PassForge creates custom worldlists based on personal target data and configurable rules in config.txt
 
-# Disclaimer
+## Disclaimer
 Do not use this to harm any real world applications
 This program is for authorized testing only!
 
-# Features:
+## Features:
 - targeted pass generation
 - configurable symbols and numbers
 - case variations
 - no duplicate pass output
 - length filtering (8-20 chars)
 - no external dependencies
+- Argparsing now allowed
 
-# Configuration (config.txt)
+## Configuration (config.txt)
 Example:
+letters = a,b,c,d,e,ff,FF,Aa
 
 numbers = 1, 2, 07, 123
 
 symbols = !, @, #, $, %
 
-# Usage
-Run : py main.py or python main.py
-Follow the prompts
+## Usage
+- py main.py -h or python main.py -h
+Flags : --firstname <name> --nickname <nickname> --birthyear <birthyear> --country <country> --number <favnumber>
+
+- Example Input:
+- py main.py --firstname john --nickname jojo --birthyear 1984 --country australia --number 61
+- Example Output
+- Generated 96144 passwords.
+- Passwords saved to wordlist.txt
+
+
 After completion program will save generated content to worldlist.txt
 
-# Plans
+## Plans
 This program is still in beta and many more feautures will be added
 
-# Author
+## Author
 Youssef Shamas
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="PassForge V1.1")
+parser.add_argument("--firstname", help="Input firstname of target", required=True)
+parser.add_argument("--nickname", help="Input nickname of target", required=True)
+parser.add_argument("--birthyear", help="Input birthyear of target", required=True)
+parser.add_argument("--country", help="Input country of target", required=True)
+parser.add_argument("--number", help="Input favorite number of target", required=True)
+
+args = parser.parse_args()
+
 def get_input():
-    first_name = str(input("Enter the target's name: ")).strip()
-    nickname = str(input("Enter the target's nickname: ")).strip()
-    birthyear = str(input("Enter the target's birthyear: ")).strip()
-    country = str(input("Enter the target's country: ")).strip()
-    favnumber = str(input("Enter the target's favnumber: ")).strip()
+    first_name = str(args.firstname).strip()
+    nickname = str(args.nickname).strip()
+    birthyear = str(args.birthyear).strip()
+    country = str(args.birthyear).strip()
+    favnumber = str(args.number).strip()
     return [first_name, nickname, birthyear, country, favnumber]
 
 def create_variations(inputs):
@@ -35,6 +46,7 @@ def read_config(filename):
     with open(filename, "r") as f:
         numbers = []
         symbols = set()
+        letters = set()
         for line in f:
             if line.startswith("#"):
                 continue
@@ -53,7 +65,7 @@ def read_config(filename):
                         try:
                             int(number)
                         except ValueError:
-                            print("You can only have numbers in numbers config")
+                            print("[-] You can only have numbers in numbers config")
                             numbers = []
                             break
                         else:
@@ -63,34 +75,55 @@ def read_config(filename):
                 for token in tokens:
                     sym = token.strip()
                     if not sym:
-                        print("No valid symbols present in the config's value")
+                        print("[-] No valid symbols present in the config's value")
                         continue
                     if len(sym) != 1:
-                        print("Symbol cannot exceeded a max length of 1")
+                        print("[-] Symbol cannot exceeded a max length of 1")
                         continue
                     if sym.isalnum() or sym.isspace():
                         continue
                     symbols.add(sym)
+            if config == "letters":
+                lettertok = value.split(",")
+                for letter in lettertok:
+                    char = letter.strip()
+                    if not char:
+                        print("[-] No valid letters in the configs value")
+                        continue
+                    if len(char) > 2:
+                        print("[-] Letter cannot exceeded a max length of 2")
+                        continue
+                    if char.isnumeric() or char.isspace():
+                        print("[-] Letter cannot be a number or space")
+                        continue
+                    letters.add(char)
+        return numbers , list(symbols), letters
 
-        return numbers , list(symbols)
 
-
-def processing(variants, numbers, symbols):
+def processing(variants, numbers, symbols, letters):
     passwords = set()
     for w1 in variants:
         for w2 in variants:
             for number in numbers:
                 for sym in symbols:
-                    p1 = w1 + number + sym + w2
-                    p2 = number + w1 + sym + w2
-                    p3 = sym + w1 + number + w2
-                    p4 = w2 + sym + w1 + number
-                    p5 = w1 + w2 + number + sym
-                    p6 = w1 + sym +  number
-                    p7 = w1 + number + sym
-                    for p in (p1,p2,p3,p4,p5,p6,p7):
-                        if 8 <= len(p) <= 20:
-                            passwords.add(p)
+                    for letter in letters:
+                        p1 = w1 + number + sym + w2 
+                        p1a = w1 + number + sym + w2 + letter
+                        p2 = number + w1 + sym + w2
+                        p2a = number + w1 + sym + w2 + letter
+                        p3 = sym + w1 + number + w2
+                        p3a = sym + w1 + number + w2 + letter
+                        p4 = w2 + sym + w1 + number
+                        p4a = w2 + sym + w1 + number + letter
+                        p5 = w1 + w2 + number + sym
+                        p5a = w1 + w2 + number + sym + letter
+                        p6 = w1 + sym +  number
+                        p6a = w1 + sym +  number + letter
+                        p7 = w1 + number + sym
+                        p7a = w1 + number + sym + letter
+                        for p in (p1,p2,p3,p4,p5,p6,p7,p1a,p2a,p3a,p4a,p5a,p6a,p7a):
+                            if 8 <= len(p) <= 20:
+                                passwords.add(p)
     return passwords
 
 def save_to_file(passwords):
@@ -101,10 +134,10 @@ def save_to_file(passwords):
         print(f"Passwords saved to wordlist.txt")
 
 def main():
-    numbers, symbols = read_config("config.txt")
+    numbers, symbols, letters = read_config("config.txt")
     inputs = get_input()
     variations = create_variations(inputs)
-    passwords = processing(variations, numbers, symbols)
+    passwords = processing(variations, numbers, symbols, letters)
     save_to_file(passwords)
 
 if __name__ == "__main__":
